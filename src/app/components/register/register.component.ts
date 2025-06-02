e1abad63a5ff7e6a12c4a52df006840f38b0a9d7
M.R.MAYUNIKA <RuwandiMayunika01@gmail.com>
2025-06-02 17:53:23 +0530

    user registration backend integration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import SHA256 from 'crypto-js/sha256';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -15,15 +19,43 @@ export class RegisterComponent {
   email = '';
   password = '';
   confirmPassword = '';
+  id = SHA256(this.email).toString();
 
   constructor(private router: Router) {}
 
-  register() {
-    if (true) {
-      this.router.navigate(['/']);
-    } else {
-      alert('Invalid credentials');
-    }
+  http = inject(HttpClient);
+
+  createUser() {
+    const user = {
+      id: this.id,
+      name: this.name,
+      email: this.email,
+      password: SHA256(this.password).toString(),
+      role: 1,
+    };
+    this.http.post('http://localhost:8080/user', user)
+      .subscribe({
+        next: (res) => {
+          console.log('User created successfully:', res);
+          alert('Registration successful!');
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.error('Error creating user:', err);
+          alert('Registration failed. Please try again.');
+        }
+      });
   }
 
+  register() {
+    if(!this.name || !this.email || !this.password || !this.confirmPassword) {
+      alert('Please fill in all fields!');
+      return; 
+    } else if(this.password !== this.confirmPassword) {
+      alert('Passwords do not match!');
+      return; 
+    } else {
+      this.createUser();  
+    }
+}
 }
